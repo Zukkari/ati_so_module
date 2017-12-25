163b31cd7ce5b71581077d743767abd6ec3393bf
Stanislav Mõškovski <stanislav.myshkovski@gmail.com>
2017-12-25 16:15:28 +0200

    stackexchange query for questions

---
+++ query.py
@@ -0,0 +1,11 @@
+from configuration import get_node_value
+import urllib.request
+
+
+def form_api_url(query):
+    return get_node_value("stack_app:url").format(
+        get_node_value("stack_app:version"),
+        get_node_value("stack_app:method"),
+        get_node_value("stack_app:site"),
+        urllib.parse.quote(query)
+    )
